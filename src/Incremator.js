44f7f88f9065971b6dfa61de023553c2c2f0eedf
Liyang Zhang <liyangz@twitter.com>
2018-10-08 11:24:49 -0700

    clean up

--- src/Incremator.js
+++ src/Incremator.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import reducers from "./reducers";
 class Incremator extends React.Component {
   render() {
     const { counter, helpMsg } = this.props;
